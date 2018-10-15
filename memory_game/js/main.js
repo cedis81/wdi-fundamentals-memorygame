a4b65feb5462b74912a8baa37c25088756c07e73
Cedric Hom <ccchom@gmail.com>
2018-10-15 12:40:59 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen","queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardFour);
+console.log("User flipped" + " " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	console.log(cardsInPlay.length);
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+	    alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}
+} else {
+	console.log("Need to draw more cards");
+}
